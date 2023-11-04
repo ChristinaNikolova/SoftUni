cfe6ec37c8960812bfff52c38fc47d1da0062819
christinanikolova <christinanikolova@github.com>
2023-11-04 15:18:29 +0200

    add advanced exercises

---
+++ C#/03. Advanced - September 2019/C# Advanced/10.MyExams/Demo exam/Skelet/HealthyHeaven/Restaurant.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HealthyHeaven
+{
+    public class Restaurant
+    {
+        private List<Salad> data;
+        private string name;
+
+        public Restaurant(string name)
+        {
+            this.Name = name;
+            this.data = new List<Salad>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                this.name = value;
+            }
+        }
+
+        public void Add(Salad salad)
+        {
+            this.data.Add(salad);
+        }
+
+        public bool Buy(string name)
+        {
+            int index = this.data.FindIndex(x => x.Name == name);
+
+            if (index == -1)
+            {
+                return false;
+            }
+
+            this.data.RemoveAt(index);
+
+            return true;
+        }
+
+        public Salad GetHealthiestSalad()
+        {
+            Salad salad = this.data
+                .OrderBy(x => x.GetTotalCalories())
+                .FirstOrDefault();
+
+            return salad;
+
+        }
+
+        public string GenerateMenu()
+        {
+            StringBuilder message = new StringBuilder();
+
+            message.AppendLine($"{this.Name} have {this.data.Count} salads:");
+
+            foreach (Salad salad in this.data)
+            {
+                message.AppendLine(salad.ToString());
+            }
+
+            return message.ToString().TrimEnd();
+
+        }
+    }
+}
---
+++ C#/03. Advanced - September 2019/C# Advanced/10.MyExams/Exam/DatingApp/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DatingApp
+{
+    class Program
+    {
+        private static object queue;
+
+        static void Main(string[] args)
+        {
+            int counterMatches = 0;
+
+            int[] malesInput = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+
+            Stack<int> males = new Stack<int>(malesInput);
+
+            int[] femalesInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> females = new Queue<int>(femalesInput);
+
+            while (females.Any() && males.Any())
+            {
+                int currentFemale = females.Peek();
+                int currentMale = males.Peek();
+
+                if (currentFemale <= 0)
+                {
+                    females.Dequeue();
+                    continue;
+                }
+
+                if (currentMale <= 0)
+                {
+                    males.Pop();
+                    continue;
+                }
+
+                if (currentFemale % 25 == 0)
+                {
+                    females.Dequeue();
+                    females.Dequeue();
+                    continue;
+                }
+
+                if (currentMale % 25 == 0)
+                {
+                    males.Pop();
+                    males.Pop();
+                    continue;
+                }
+
+                if (currentFemale == currentMale)
+                {
+                    counterMatches++;
+                    females.Dequeue();
+                    males.Pop();
+                }
+                else
+                {
+                    females.Dequeue();
+                    currentMale -= 2;
+
+                    males.Pop();
+                    males.Push(currentMale);
+                }
+            }
+
+            Console.WriteLine($"Matches: {counterMatches}");
+
+            if (males.Any())
+            {
+                Console.WriteLine($"Males left: {string.Join(", ", males)}");
+            }
+            else
+            {
+                Console.WriteLine("Males left: none");
+            }
+
+            if (females.Any())
+            {
+                Console.WriteLine($"Females left: {string.Join(", ", females)}");
+            }
+            else
+            {
+                Console.WriteLine("Females left: none");
+            }
+        }
+    }
+}
