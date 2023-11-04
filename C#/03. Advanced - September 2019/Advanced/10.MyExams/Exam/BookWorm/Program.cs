f0675cff84c4eb3e73605fcda0bc45266a7a4a54
christinanikolova <christinanikolova@github.com>
2023-11-04 15:25:53 +0200

    rename folder

---
+++ C#/03. Advanced - September 2019/Advanced/10.MyExams/Exam/BookWorm/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace BookWorm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder message = new StringBuilder();
+            message.Append(text);
+
+            int sizeMatrix = int.Parse(Console.ReadLine());
+
+            char[][] matrix = new char[sizeMatrix][];
+
+            int playerRow = 0;
+            int playerCol = 0;
+
+            for (int row = 0; row < sizeMatrix; row++)
+            {
+                matrix[row] = Console.ReadLine()
+                    .ToCharArray();
+
+                if (matrix[row].Contains('P'))
+                {
+                    playerRow = row;
+                    playerCol = matrix[row].ToList().IndexOf('P');
+
+                    matrix[playerRow][playerCol] = '-';
+                }
+            }
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string command = input;
+
+                if (command == "left")
+                {
+                    if (playerCol - 1 >= 0)
+                    {
+                        playerCol--;
+
+                        if (char.IsLetter(matrix[playerRow][playerCol]))
+                        {
+                            char letter = matrix[playerRow][playerCol];
+                            message.Append(letter);
+
+                            matrix[playerRow][playerCol] = '-';
+                        }
+                    }
+                    else
+                    {
+                        if (message.Length > 0)
+                        {
+                            message.Length--;
+                        }
+                    }
+                }
+                else if (command == "right")
+                {
+                    if (playerCol + 1 <= sizeMatrix - 1)
+                    {
+                        playerCol++;
+
+                        if (char.IsLetter(matrix[playerRow][playerCol]))
+                        {
+                            char letter = matrix[playerRow][playerCol];
+                            message.Append(letter);
+
+                            matrix[playerRow][playerCol] = '-';
+                        }
+                    }
+                    else
+                    {
+                        if (message.Length > 0)
+                        {
+                            message.Length--;
+                        }
+                    }
+                }
+                else if (command == "up")
+                {
+
+                    if (playerRow - 1 >= 0)
+                    {
+                        playerRow--;
+
+                        if (char.IsLetter(matrix[playerRow][playerCol]))
+                        {
+                            char letter = matrix[playerRow][playerCol];
+                            message.Append(letter);
+
+                            matrix[playerRow][playerCol] = '-';
+                        }
+                    }
+                    else
+                    {
+                        if (message.Length > 0)
+                        {
+                            message.Length--;
+                        }
+                    }
+                }
+                else if (command == "down")
+                {
+                    if (playerRow + 1 <= sizeMatrix - 1)
+                    {
+                        playerRow++;
+
+                        if (char.IsLetter(matrix[playerRow][playerCol]))
+                        {
+                            char letter = matrix[playerRow][playerCol];
+                            message.Append(letter);
+
+                            matrix[playerRow][playerCol] = '-';
+                        }
+                    }
+                    else
+                    {
+                        if (message.Length > 0)
+                        {
+                            message.Length--;
+                        }
+                    }
+                }
+            }
+
+            matrix[playerRow][playerCol] = 'P';
+
+            Console.WriteLine(message);
+
+            foreach (char[] row in matrix)
+            {
+                Console.WriteLine(string.Join("", row));
+            }
+        }
+    }
+}
---
+++ C#/03. Advanced - September 2019/Advanced/10.MyExams/Exam/Rabbits/Cage.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rabbits
+{
+    public class Cage
+    {
+        private List<Rabbit> data;
+        private string name;
+        private int capacity;
+
+        public Cage(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.data = new List<Rabbit>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                this.name = value;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return this.capacity;
+            }
+            private set
+            {
+                this.capacity = value;
+            }
+        }
+
+        public int Count => this.data.Count;
+
+        public void Add(Rabbit rabbit)
+        {
+            if (this.data.Count + 1 <= this.Capacity)
+            {
+                this.data.Add(rabbit);
+            }
+        }
+
+        public bool RemoveRabbit(string name)
+        {
+            int index = this.data.FindIndex(x => x.Name == name);
+
+            if (index== -1)
+            {
+                return false;
+            }
+
+            this.data.RemoveAt(index);
+
+            return true;
+        } 
+
+        public void RemoveSpecies(string species)
+        {
+            this.data.RemoveAll(x => x.Species == species);
+        }
+
+        public Rabbit SellRabbit(string name)
+        {
+            Rabbit rabbit = this.data.FirstOrDefault(x => x.Name == name);
+
+            rabbit.Available = false;
+
+            return rabbit;
+        } 
+
+        public Rabbit[] SellRabbitsBySpecies(string species)
+        {
+            List<Rabbit> rabbits = new List<Rabbit>();
+
+            foreach (Rabbit rabbit in this.data)
+            {
+                if (rabbit.Species == species)
+                {
+                    rabbit.Available = false;
+                    rabbits.Add(rabbit);
+                }
+            }
+
+            return rabbits.ToArray();
+        }
+
+        public string Report()
+        {
+            StringBuilder message = new StringBuilder();
+
+            message.AppendLine($"Rabbits available at {this.Name}:");
+
+            foreach (Rabbit rabbit in this.data)
+            {
+                if (rabbit.Available == true)
+                {
+                    message.AppendLine(rabbit.ToString());
+                }
+            }
+
+            return message.ToString().TrimEnd();
+        }
+    }
+}
